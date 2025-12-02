2fa4ed1c6037be208d36e5e9c40971cac66e6daa
ari-el7 <akbar_23510018@stimata.ac.id>
2025-12-02 15:20:39 +0700

    IntegrasiAPI - Create

---
+++ app/src/main/java/com/example/latihan_api/CreateCatatan.kt
@@ -0,0 +1,74 @@
+package com.example.latihan_api
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.latihan_api.databinding.ActivityCreateCatatanBinding
+import com.example.latihan_api.entities.Catatan
+import kotlinx.coroutines.launch
+
+class CreateCatatan : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateCatatanBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityCreateCatatanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setupEvents()
+    }
+
+    fun setupEvents() {
+        binding.tombolSimpan.setOnClickListener {
+            val judul = binding.inputJudul.text.toString()
+            val isi = binding.inputIsi.text.toString()
+
+            if (judul.isEmpty() || isi.isEmpty()) {
+                displayMessage("Judul dan isi catatan harus diisi")
+                return@setOnClickListener
+            }
+
+            // user_id FIXED → 1
+            val payload = Catatan(
+                judul = judul,
+                isi = isi,
+                user_id = 1,
+                id = null
+            )
+
+            lifecycleScope.launch {
+                val response = RetrofitClient.catatanRepository.createCatatan(payload)
+
+                if (response.isSuccessful) {
+                    displayMessage("Catatan berhasil dibuat")
+
+                    startActivity(Intent(this@CreateCatatan, MainActivity::class.java))
+                    finish()
+
+                } else {
+                    val errorText = response.errorBody()?.string()
+                    Log.e("API_ERROR", "Error: $errorText")
+                    displayMessage("Gagal: $errorText")
+                }
+            }
+        }
+    }
+
+    fun displayMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
